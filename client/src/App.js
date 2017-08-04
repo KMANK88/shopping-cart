fe7a950df1e03563d4a5599b5c1bb2fac30ed36f
Karsten Pease <karstenpease88@outlook.com>
2017-08-04 13:09:13 -0600

    added NavBar

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
 import {HomeContainer , AboutContainer, StoreContainer} from './containers';
+import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import {NavBar} from "./components";
 
 import './App.css';
 
 class App extends Component {
+
+
   render() {
     return (
+  <div>
+      <Router>
       <div>
-        <HomeContainer />
-        <AboutContainer />
-        <StoreContainer />
+        <div>
+          <NavBar />
+        </div>
+        <div>
+          <Route exact path="/" component={ HomeContainer } />
+          <Route path="/my-about-page" component={ AboutContainer } />
+          <Route path="/my-shop-page" component={ StoreContainer } />
+        </div>
       </div>
+      </Router>
+
+    </div>
     );
   }
 }
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,3 +1,4 @@
 export {default as Home} from './home/Home';
 export {default as About} from './about/About';
 export {default as Store} from './store/Store';
+export {default as NavBar} from './navBar/NavBar';
